aeaac3230e08d9c2eb3af1d9917bad591106da1c
Craig Freeman <craigfreeman@gmail.com>
2015-03-24 13:21:07 -0600

    Updated favicons

--- wp-content/themes/cfree-v4/header.php
+++ wp-content/themes/cfree-v4/header.php
@@ -17,7 +17,23 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo home_url(); ?>/favicons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo home_url(); ?>/favicons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo home_url(); ?>/favicons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo home_url(); ?>/favicons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo home_url(); ?>/favicons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo home_url(); ?>/favicons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo home_url(); ?>/favicons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo home_url(); ?>/favicons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo home_url(); ?>/favicons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo home_url(); ?>/favicons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo home_url(); ?>/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo home_url(); ?>/favicons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo home_url(); ?>/favicons/favicon-16x16.png">
+<link rel="manifest" href="<?php echo home_url(); ?>/manifest.json">
+<meta name="msapplication-TileColor" content="#F1F1F1">
+<meta name="msapplication-TileImage" content="<?php echo home_url(); ?>/favicons/ms-icon-144x144.png">
+<meta name="theme-color" content="#F1F1F1">
 
 <?php wp_head(); ?>
 
