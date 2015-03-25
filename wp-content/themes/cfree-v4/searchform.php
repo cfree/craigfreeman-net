c673201369c561ce169cc7be915f696d75493e19
Craig Freeman <craigfreeman@gmail.com>
2015-03-25 12:42:19 -0600

    Corrected search action

--- wp-content/themes/cfree-v4/searchform.php
+++ wp-content/themes/cfree-v4/searchform.php
@@ -5,7 +5,7 @@
  * @package cfree
  */
 ?>
-<form class="search-form" action="http://craigfreeman.dev/">
+<form class="search-form" action="<?php echo home_url(); ?>">
 	<label for="s"><?php _e( 'Search site:', 'cfree' ); ?></label>
 	<input type="text" name="s">
 	<button class="search-button"><?php _e( 'Search', 'cfree' ); ?></button>
