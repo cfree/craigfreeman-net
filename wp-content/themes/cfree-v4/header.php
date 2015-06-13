fdfe4bfa3ac6b776d0b2efa9b825cf41aff04c82
Craig Freeman <craigfreeman@gmail.com>
2015-06-13 14:41:51 -0600

    Added Google site verification

--- wp-content/themes/cfree-v4/header.php
+++ wp-content/themes/cfree-v4/header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="google-site-verification" content="0WxNuolI-hVRiZmWEw41DhyUpTFkyq-WTFqqdYS-o7g" />
 
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
